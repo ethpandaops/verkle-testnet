3ab1848259708385b9f369ab00ec0355f57488bb
parithosh <parithosh.jayanthi@ethereum.org>
2022-09-26 12:08:38 +0200

    updating verkle playbooks

---
+++ beverly-hills-testnet/landing_page_data/custom_text.md
@@ -0,0 +1,15 @@
+## General information
+enode for execution clients:
+```sh
+- "enode://bc57276085419e3dd13ed4f4ba14d6fe513582d25e6b8fbf830f5e331d5911348c6df4f776685bb83eb6948f42697cf0941b6f58796872be426b9206ed15946e@206.189.52.190:30303"
+```
+
+## Genesis configuration
+### Execution layer clients
+
+```sh
+https://github.com/parithosh/verkle-testnet/blob/main/verkle-testnet/custom_config_data/genesis.json
+```
+
+You can optimise the `geth init` time by using our precompiled data found [here](https://beverlyhills.fra1.digitaloceanspaces.com/precomp). 
+Just place it in the same location as the `genesis.json` file of geth and perform all the steps normally. 
---
+++ terraform/environment/beverly-hills-testnet/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    endpoint                    = "https://fra1.digitaloceanspaces.com"
+    region                      = "us-east-1"
+    bucket                      = "verkle-testnets"
+    key                         = "infrastructure/beverly-hills-testnet/terraform.tfstate"
+  }
+}
+
+
+locals {
+  ssh_key_name = "parithosh"
+  digital_ocean_project_name = "Verkle Testnets"
+  size = "s-4vcpu-8gb-amd"
+  region = "fra1"
+  image = "ubuntu-20-04-x64"
+  name = "beverlyHills"
+  vpc_name="beverlyHills"
+  vpc_ip_range="10.169.77.0/24"
+  vpc_region = "fra1"
+  shared_project_tags = [
+                            "Owner:Parithosh",
+                        ]
+
+}
+
+resource "digitalocean_vpc" "vpc" {
+  name = local.vpc_name
+  ip_range = local.vpc_ip_range
+  region = local.vpc_region
+}
+
+
+module "verkle_bootnode" {
+  droplet_count = 1
+
+  size =  "s-4vcpu-8gb-amd"
+  region = local.region
+  image = local.image
+  name = "verkle-bootnode"
+  source = "../../modules/"
+
+  tags = concat(local.shared_project_tags,["execution","geth","explorer","bootnode","faucet", "landing_page","reverse_proxy","beverly-hills-testnet"])
+  ssh_key_name = local.ssh_key_name
+  digital_ocean_project_name = local.digital_ocean_project_name
+#  vpc_uuid = digitalocean_vpc.vpc.id
+}
+
+module "verkle-geth" {
+  droplet_count = 2
+
+  size =  "s-2vcpu-4gb-amd"
+  region = local.region
+  image = local.image
+  name = "verkle-geth"
+  source = "../../modules/"
+
+  tags = concat(local.shared_project_tags,["execution","geth","beverly-hills-testnet"])
+  ssh_key_name = local.ssh_key_name
+  digital_ocean_project_name = local.digital_ocean_project_name
+  #  vpc_uuid = digitalocean_vpc.vpc.id
+}
