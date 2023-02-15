278469ebc563bcfad06e064bd7b53cda50bd13ce
Barnabas Busa <busa.barnabas@gmail.com>
2023-02-15 11:37:43 +0100

    add bverly-sepolia-shadowfork-1

---
+++ terraform/environment/beverly-hills-sepolia-shadowfork-1/ethpandops-io.tf
@@ -0,0 +1,26 @@
+resource "cloudflare_record" "dns-entry" {
+  for_each = {
+    for vm in local.digitalocean_vms : "${vm.id}" => vm
+  }
+  zone_id = data.sops_file.cloudflare.data["zones.ethpandaops-io.zone_id"]
+  name    = "${each.value.name}.${data.sops_file.cloudflare.data["zones.ethpandaops-io.domain"]}"
+  type    = "A"
+  value   = "${digitalocean_droplet.main[each.value.id].ipv4_address}"
+  proxied = false
+}
+
+resource "cloudflare_record" "dns-entry-bootnode" {
+  zone_id = data.sops_file.cloudflare.data["zones.ethpandaops-io.zone_id"]
+  name    = "beverly-hills-shadowfork-1.${data.sops_file.cloudflare.data["zones.ethpandaops-io.domain"]}"
+  type    = "A"
+  value   = "${digitalocean_droplet.main["teku-geth.bootnode"].ipv4_address}"
+  proxied = false
+}
+
+resource "cloudflare_record" "dns-entry-bootnode-wildcard" {
+  zone_id = data.sops_file.cloudflare.data["zones.ethpandaops-io.zone_id"]
+  name    = "*.beverly-hills-shadowfork-1"
+  type    = "CNAME"
+  value   = "beverlyhills.${data.sops_file.cloudflare.data["zones.ethpandaops-io.domain"]}"
+  proxied = false
+}
