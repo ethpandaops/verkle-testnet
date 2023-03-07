6a544347cbe35c2c2f42cfca35c1c849dbd52b39
parithosh <parithosh.jayanthi@ethereum.org>
2023-03-07 15:24:11 +0100

    relaunched beverly hills

---
+++ beverly-hills-testnet/custom_config_data/README.md
@@ -0,0 +1,22 @@
+```
+beverly-hills-teku-geth-1: 0 - 16
+beverly-hills-teku-geth-2: 16 - 32
+beverly-hills-teku-geth-3: 32 - 48
+beverly-hills-teku-geth-4: 48 - 64
+beverly-hills-teku-geth-5: 64 - 80
+beverly-hills-teku-geth-6: 80 - 96
+beverly-hills-teku-geth-7: 96 - 112
+beverly-hills-teku-geth-8: 112 - 128
+beverly-hills-lighthouse-geth-1: 128 - 144
+beverly-hills-lighthouse-geth-2: 144 - 160
+beverly-hills-lighthouse-geth-3: 160 - 176
+beverly-hills-lighthouse-geth-4: 176 - 192
+beverly-hills-lighthouse-geth-5: 192 - 208
+beverly-hills-lighthouse-geth-6: 208 - 224
+beverly-hills-lodestar-geth-1: 224 - 240
+beverly-hills-lodestar-geth-2: 240 - 256
+beverly-hills-lodestar-geth-3: 256 - 272
+beverly-hills-lodestar-geth-4: 272 - 288
+beverly-hills-lodestar-geth-5: 288 - 304
+beverly-hills-lodestar-geth-6: 304 - 320
+```
--- beverly-hills-testnet/generate_keys.sh
+++ beverly-hills-testnet/generate_keys.sh
@@ -14,9 +14,6 @@ function prep_group {
   let keys_to_create=$4
   naming_prefix="$5"
   validators_per_host=$6
-  el_address=$(cat inventory/group_vars/all.yaml | yq .fee_recipient)
-  genesis_root=$(cat custom_config_data/parsedBeaconState.json | jq -r .genesis_validators_root)
-  genesis_version=$(cat custom_config_data/config.yaml |  yq .GENESIS_FORK_VERSION)
   echo "Group base: $group_base"
   for (( i = 0; i < keys_to_create; i++ )); do
     let node_index=group_base+i
@@ -31,14 +28,6 @@ function prep_group {
     --source-max="$validators_source_max" \
     --source-min="$validators_source_min" \
     --source-mnemonic="$validators_source_mnemonic"
-    eth2-val-tools bls-address-change \
-    --withdrawals-mnemonic="$validators_source_mnemonic" \
-    --execution-address="$el_address" \
-    --source-min="$validators_source_min" \
-    --source-max="$validators_source_max" \
-    --genesis-validators-root=$genesis_root \
-    --fork-version="$genesis_version" \
-    --as-json-list=true > "validator_prep/$naming_prefix-$node_index/change_operations.json"
   done
 }
 
